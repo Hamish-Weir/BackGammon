dafe1c8d72e875a17c2df8fecd72cb8785c7995e
c93350hw <hamish.weir@student.manchester.ac.uk>
2026-03-10 13:04:42 +0000

    player and game adapted to chance

---
+++ ChanceReducedGame/src/Game.py
@@ -0,0 +1,192 @@
+import logging
+import os
+import sys
+from time import perf_counter_ns as time
+import random
+
+from src.AgentBase import AgentBase
+from src.Player import Player
+from src.Board import BOARD_START, DIE_SIZE, P1LOGICALOFF, P2LOGICALOFF, Board, P1BAR,P2BAR,P1OFF,P2OFF
+
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(
+    format="[%(levelname)s]-%(asctime)s - %(message)s", level=logging.INFO
+)
+
+class Game:
+    _turn:int
+    MAXIMUM_TIME = 5 * 60 *(10**9)
+
+    def __init__(
+        self,
+        player1: Player,
+        player2: Player,
+        silent: bool = False,
+        timelimitOff = False
+    ):
+        self._turn = 0
+        self._start_time = time()
+        self.random = random.randint
+
+        self._board = Board()
+        self._player_board = Board()
+
+        self.current_player = 1
+        self.player1 = player1
+        self.player2 = player2
+        
+        self.players = {
+            1: self.player1,
+            -1: self.player2,
+        }
+
+        
+        self.timelimitOff = timelimitOff
+        logDest = sys.stderr
+        if silent:
+            logger.setLevel(logging.CRITICAL)
+            logDest = os.devnull
+
+        if logDest != sys.stderr:
+            self.logDest = open(logDest, "w")
+        else:
+            self.logDest = logDest
+
+    @property
+    def turn(self):
+        return self._turn
+
+    @property
+    def board(self):
+        return self._board
+
+    @staticmethod
+    def m_to_str(m,player):
+        if m[0] == P1BAR or m[0] == P2BAR:
+                s = "BAR"
+        else:
+            s = f"{m[0]-BOARD_START+1:3d}"
+
+        if Board.end_point(m[0],m[1],player) == P1OFF or Board.end_point(m[0],m[1],player) == P2OFF:
+            e = "OFF"
+        else:
+            e = f"{Board.end_point(m[0],m[1],player)-BOARD_START+1:3d}"
+
+        m_str = str(f"({s}, {e}, {m[1]}), ")
+        return m_str
+
+    @staticmethod
+    def ms_to_str(ms,player):
+        ms_str = "["
+        for m in ms:
+            m_str = Game.m_to_str(m,player)
+            ms_str += m_str
+        ms_str += "]"    
+        return ms_str  
+
+
+    def run(self):
+        assert issubclass(type(self.players[1].agent), AgentBase)
+        assert issubclass(type(self.players[-1].agent), AgentBase)
+
+        logger.info("Game started")
+        self._play()
+
+        if self.logDest != sys.stderr:
+            self.logDest.close()
+
+        print(self.board)
+
+        return self.board.get_winner()
+    
+    def _play(self):
+
+        opponentMove = None
+        
+        while True:
+            self._turn += 1
+
+            currentPlayer: Player = self.players[self.current_player]
+            playerAgent = currentPlayer.agent
+
+            self._player_board.set(self.board.get())
+            die1 = self.random(1,DIE_SIZE)
+            die2 = self.random(1,DIE_SIZE)
+
+
+            logger.info(f"Turn {self.turn}: player {currentPlayer.name}")
+            logger.info(f"Starting Board:\n{str(self.board)}")
+            logger.info(f"Dice Roll: ({die1}, {die2})")
+
+            
+
+            start = time()
+            ms = playerAgent.make_move(self._player_board, die1, die2, self.turn, opponentMove)
+            end = time()
+
+            if not self._player_board == self._board:
+                raise Exception("Player Modified Board")
+
+            currentPlayer.move_time += end - start
+
+            logger.debug(
+                f"Player {currentPlayer.name}; Move time: {Game.ns_to_s(currentPlayer.move_time)}s"
+            )
+            logger.info(f"Player: {currentPlayer.name}; Move Sequence: {Game.ms_to_str(ms,self.current_player)}")
+
+            if not self.timelimitOff:
+                if currentPlayer.move_time > Game.MAXIMUM_TIME:
+                    logger.info(f"Player {currentPlayer.name} timed out ({currentPlayer.move_time})")
+                    raise Exception("Player Timed Out")
+            
+            if self.is_valid_move_sequence(ms, die1, die2):
+                logger.debug("Move is valid")
+                self._make_move(ms)
+                opponentMove = ms
+            else:
+                logger.info(f"Player {currentPlayer.name} made an illegal move")
+                raise Exception("Illegal Move")
+            
+            if self.board.get_winner() != 0:
+                break
+
+            
+            logger.info("")
+            self.current_player = -self.current_player
+
+        return self._end_game()
+
+    def _end_game(self):
+
+        total_time = time() - self._start_time
+
+        logger.info("Game over")
+        logger.info(f"Final Board:\n{str(self.board)}")
+        logger.info(f"Total time: {Game.ns_to_s(total_time)}s")
+
+        logger.info(f"Player {self.players[self.current_player].name} has won")
+        winner = self.players[self.current_player].name
+
+        for p in self.players.values():
+            print(f"{p.name},{p.move_time}", file=self.logDest)
+        print(f"winner,{winner}", file=self.logDest)
+        logger.info(f"Total Game Time: {Game.ns_to_s(total_time)}s")
+
+    def is_valid_move_sequence(self, ms, die1, die2):
+        return True
+
+    def _make_move(self, ms):
+        """Performs a valid move on the board, then prints its results."""
+
+        logger.debug(f"Move made: {ms}")
+        current_player = self.players[self.current_player]
+
+        self.board.do_move_sequence(ms,self.current_player)
+
+
+  
+
+    def ns_to_s(t):
+        """Method for standardised nanosecond to second conversion."""
+        return int(t / 10**6) / 10**3
